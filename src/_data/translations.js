24c907a79226646b2335e0d735fda2058a23ecd1
fershad <27988517+fershad@users.noreply.github.com>
2023-09-13 06:47:46 +0800

    Synced by Front Matter

--- src/_data/translations.js
+++ src/_data/translations.js
@@ -23,28 +23,28 @@ module.exports = {
         dark: 'Dark',
         light: 'Light'
     },
-    sv: {
-        meta: {
-            title: 'elva',
-            rssTitle: 'RSS Feed',
-            jsonTitle: 'JSON Feed',
-            description: 'A multilingual clean green 11ty starter theme',
-            opengraphDefaultAlt: 'A Swedish description of the default opengraph image'
-        },
-        header: {
-            skipLink: 'Hoppa till innehållet',
-            home: 'Hem'
-        },
-        readingTime: {
-            underMinute: 'Mindre än 1 minut att läsa',
-            minute: '1 minut till läsaren',
-            other: '{{ minutes }} minuter att läsa'
-        },
-        feeds: {
-            info: 'Detta är ett RSS feed. Kopiera och klistra in webbadressen i din flödesläsare. Besök <a href="https://aboutfeeds.com">About Feeds</a> för att lära dig mer om RSS.',
-            title: 'Nyligen publicerad'
-        },
-        dark: 'Mörk',
-        light: 'Ljus'
-    }
+    // sv: {
+    //     meta: {
+    //         title: 'elva',
+    //         rssTitle: 'RSS Feed',
+    //         jsonTitle: 'JSON Feed',
+    //         description: 'A multilingual clean green 11ty starter theme',
+    //         opengraphDefaultAlt: 'A Swedish description of the default opengraph image'
+    //     },
+    //     header: {
+    //         skipLink: 'Hoppa till innehållet',
+    //         home: 'Hem'
+    //     },
+    //     readingTime: {
+    //         underMinute: 'Mindre än 1 minut att läsa',
+    //         minute: '1 minut till läsaren',
+    //         other: '{{ minutes }} minuter att läsa'
+    //     },
+    //     feeds: {
+    //         info: 'Detta är ett RSS feed. Kopiera och klistra in webbadressen i din flödesläsare. Besök <a href="https://aboutfeeds.com">About Feeds</a> för att lära dig mer om RSS.',
+    //         title: 'Nyligen publicerad'
+    //     },
+    //     dark: 'Mörk',
+    //     light: 'Ljus'
+    // }
 }
