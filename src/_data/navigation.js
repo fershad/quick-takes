d80b6c6e4067fc75bc637abd7d318e16457e7912
fershad <27988517+fershad@users.noreply.github.com>
2023-09-13 08:14:29 +0800

    Synced by Front Matter

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,17 +1,17 @@
-module.exports = {
-    en: [
-		{
-			text: 'Home',
-			url: '/'
-		},
-		{
-			text: 'About',
-			url: '/about/'
-		},
-		{
-			text: 'GitHub',
-			url: 'https://github.com/scottsweb/elva'
-		}
-    ],
-};
+// module.exports = {
+//     en: [
+// 		{
+// 			text: 'Home',
+// 			url: '/'
+// 		},
+// 		{
+// 			text: 'About',
+// 			url: '/about/'
+// 		},
+// 		{
+// 			text: 'GitHub',
+// 			url: 'https://github.com/scottsweb/elva'
+// 		}
+//     ],
+// };
   
---
+++ src/posts/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello, world.
+date: 2023-09-13
+draft: true
+lang: "en"
+type: post
+layout: post
+---
+
+This is a test post.
